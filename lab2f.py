5955a2f9f39e658efbf7e8c8be46ed966fd5db54
Auneeb Khan <akhan339@myseneca.ca>
2026-05-24 14:46:13 -0400

    push lab2f

---
+++ lab2f.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+#Author: Auneeb Kha 
+#Author ID: akhan339
+#Date Created: 2026/05/24
+
+import sys 
+
+timer = int(sys.argv[1])
+
+while timer != 0:
+    print(timer) 
+    timer = timer -1 
+
+print('blast off!')
